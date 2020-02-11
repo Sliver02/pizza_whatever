aa9c73bc35e29711ed11459853a31b23fc5aa606
JPanzera <J.Panzera@eu.aswatson.com>
2020-02-11 16:17:57 +0100

    merged finally

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 // Global App Controller
 import '../scss/main';
-import * as scroll from './scrollSmooth';
+import scroll from './scrollSmooth';
 import Search from './models/Search';
 
 /** Global state of the app
@@ -24,7 +24,7 @@ const controlSearch = async () => {
         await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
+        // console.log(state.search.result);
     }
 };
 
@@ -34,28 +34,12 @@ const controlSearch = async () => {
 // });
 
 const search = new Search('pizza');
-// console.log(search);
+console.log(search);
 search.getResults();
 
-// import axios from 'axios';
-
-// Models
-// import str from './models/search';
-// import * as searchView from './views/searchView';
-
-// console.log(`Imported functions: ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3,5)}. ${str}`);
-// API url https://forkify-api.herokuapp.com
-
-// async function getResults(query) {
-//     const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-//     const recipes = res.data.recipes;
-//     console.log(recipes);
-// }
-
-// getResults('pizza');
 
 function multiplyPreview(count, deep) {
-    var node = document.querySelector('.recipes__preview');
+    var node = document.querySelector('.recipe');
 
     for (var i = 0, copy; i < count - 1; i++) {
         copy = node.cloneNode(deep);
@@ -66,22 +50,22 @@ function multiplyPreview(count, deep) {
 function addSlide(count, deep) {
     var node = document.querySelector('.slide');
 
-    window.alert('WIP 12 max-recipes for Now!')
+    window.alert('WIP 12 max-recipes for Now!');
 
-    for (var i=0; i < Math.round(count/12); i++) {
+    // for (var i=0; i < Math.round(count/12); i++) {
 
-        // copy = node.cloneNode(deep);
-        // node.parentNode.insertBefore(copy, node);
+    //     copy = node.cloneNode(deep);
+    //     node.parentNode.insertBefore(copy, node);
 
-        // // Get the last <li> element ("Milk") of <ul> with id="myList2"
-        // var itm = document.getElementById("myList2").lastChild;
+    //     // Get the last <li> element ("Milk") of <ul> with id="myList2"
+    //     var itm = document.getElementById("myList2").lastChild;
 
-        // // Copy the <li> element and its child nodes
-        // var cln = itm.cloneNode(true);
+    //     // Copy the <li> element and its child nodes
+    //     var cln = itm.cloneNode(true);
 
-        // // Append the cloned <li> element to <ul> with id="myList1"
-        // document.getElementById("myList1").appendChild(cln); 
-    }
+    //     // Append the cloned <li> element to <ul> with id="myList1"
+    //     document.getElementById("myList1").appendChild(cln); 
+    // }
 
 }
 
@@ -90,10 +74,10 @@ function displayPreview(count) {
     if (count <= 12) {
         multiplyPreview(count, true);
     } else {
-        addSlide(count, true)
+        addSlide(count, true);
     }
 
     scroll.scrollSections();
 }
 
-displayPreview(8);
+displayPreview(12);
--- src/js/scrollSmooth.js
+++ src/js/scrollSmooth.js
@@ -1,70 +1,46 @@
-//var currentSection = 0;
-// var start = document.getElementById('start');
-// var recipes = document.getElementById('recipes');
+ export default {
+     
+    scrollSections() {
+        // Array of all elements with section class
+        var sections = document.querySelectorAll('.section');
 
-// var btnNext = start.getElementsByClassName('btn--next')[0];
-// var btnPrev = recipes.getElementsByClassName('btn--prev')[0];
+        // loop trhough each section to pass click events
+        sections.forEach((section, index) => {
 
-// console.log(btnFrwd);
-// console.log(btnPrev);
+            // Select dynamically the 
+            var next = section.querySelectorAll('.btn--next');
+            var prev = section.querySelectorAll('.btn--prev');
 
-// btnFrwd.addEventListener('click', (e) => {
-//     console.log('sei un coglione');
-//     recipes.scrollIntoView({ 
-//         behavior: 'smooth' 
-//     });
-// });
+            next.forEach((btnNext) => {
 
-// btnPrev.addEventListener('click', (e) => {
-//     console.log('sei un coglione ma ALLINSU');
-//     start.scrollIntoView({ 
-//         behavior: 'smooth' 
-//     });
-// });
-
-
-export function scrollSections() {
-    // Array of all elements with section class
-    var sections = document.querySelectorAll('.section');
-
-    // loop trhough each section to pass click events
-    sections.forEach((section, index) => {
-
-        // Select dynamically the 
-        var next = section.querySelectorAll('.btn--next');
-        var prev = section.querySelectorAll('.btn--prev');
-
-        console.log(next);
-
-        next.forEach((btnNext) => {
-
-            if (index <= sections.length && btnNext != undefined) {
-        
-                var nextSection = sections[index + 1];
-                
-                btnNext.addEventListener('click', (e) => {
-                    console.log('asd');
-                    history.pushState({"id":nextSection.id}, nextSection.id, `/#${nextSection.id}`);
-                    nextSection.scrollIntoView({
-                        behavior: 'smooth'
+                if (index <= sections.length && btnNext != undefined) {
+            
+                    var nextSection = sections[index + 1];
+                    
+                    btnNext.addEventListener('click', (e) => {
+                        history.pushState({"id":nextSection.id}, nextSection.id, `/#${nextSection.id}`);
+                        nextSection.scrollIntoView(true, {
+                            behavior: 'smooth'
+                        });
                     });
-                });
-            }
-        });
+                }
+            });
 
 
-        prev.forEach((btnPrev) => {
+            prev.forEach((btnPrev) => {
 
-            if (index >= 0 && btnPrev != undefined) {
-                var prevSection = sections[index - 1];
+                if (index >= 0 && btnPrev != undefined) {
+                    var prevSection = sections[index - 1];
 
-                btnPrev.addEventListener('click', (e) => {
-                    history.pushState({"id":prevSection.id}, prevSection.id, `/#${prevSection.id}`);
-                    prevSection.scrollIntoView({
-                        behavior: 'smooth'
+                    btnPrev.addEventListener('click', (e) => {
+                        history.pushState({"id":prevSection.id}, prevSection.id, `/#${prevSection.id}`);
+                        prevSection.scrollIntoView(true, {
+                            behavior: 'smooth'
+                        });
                     });
-                });
-            }
+                }
+            });
         });
-    });
+    }
+
 }
